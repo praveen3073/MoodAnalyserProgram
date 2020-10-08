8b570623fdfc73b7d82938c4eba07e33494000ce
praveen3073 <pop3073@gmail.com>
2020-10-08 09:36:24 +0530

    Modified: UC3 Throw Only Exception, Don't Catch in analyseMood()

--- main/java/com/moodanalyser/MoodAnalyser.java
+++ main/java/com/moodanalyser/MoodAnalyser.java
@@ -1,5 +1,7 @@
 package com.moodanalyser;
 
+import java.util.Scanner;
+
 enum MoodAnalyserErrorMessage {
     NULLMESSAGE("Null Mood Error"), EMPTYMESSAGE("Empty Mood Error");
     private final String message;
@@ -25,21 +27,26 @@ public class MoodAnalyser {
     }
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         System.out.println("Welcome to Mood Analyser Program");
+        try{
+            System.out.print("Enter mood: ");
+            String moodMessage = in.nextLine();
+            MoodAnalyser moodAnalyser = new MoodAnalyser(moodMessage);
+            System.out.println(moodAnalyser.analyseMood());
+        }catch(MoodAnalysisException e){
+            System.out.println("Output: HAPPY\n" + e.getMessage());
+        }
     }
 
     protected String analyseMood() throws MoodAnalysisException {
-        try {
-            if (message.isEmpty()) {
-                throw new MoodAnalysisException(MoodAnalyserErrorMessage.EMPTYMESSAGE.getMessage(), "HAPPY");
-            } else if (message.contains("Sad"))
-                return "SAD";
-            else
-                return "HAPPY";
-        } catch (NullPointerException e) {
-            throw new MoodAnalysisException(MoodAnalyserErrorMessage.NULLMESSAGE.getMessage(), "HAPPY");
-        } catch (MoodAnalysisException e) {
-            throw e;
-        }
+        if(message==null)
+            throw new MoodAnalysisException(MoodAnalyserErrorMessage.NULLMESSAGE.getMessage());
+        else if (message.isEmpty())
+            throw new MoodAnalysisException(MoodAnalyserErrorMessage.EMPTYMESSAGE.getMessage());
+        else if (message.contains("Sad"))
+            return "SAD";
+        else
+            return "HAPPY";
     }
 }
